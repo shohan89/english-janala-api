1c4a1e1f0462902b0d3dd7b61a60c1d286d7eaa5
shohan89 <your-maffin.killer007@gmail.com>
2026-02-12 21:48:53 +0600

    Load Words by Level and Display Cards

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,41 @@ const loadLevels = async () => {
     }
 }
 
+// load level wise words via ID
+const loadLevelWords = async levelNo => {
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/level/${levelNo}`);
+        const data = await res.json();
+        const words = data.data;
+        displayLevelWords(words);
+    } catch (error) {
+        console.error('Error fetching lesson words: ', error);
+    }
+}
+
+// show level words
+const displayLevelWords = words => {
+    const wordContainer = document.getElementById('wordContainer');
+    // clear wordContainer section
+    wordContainer.innerHTML = '';
+    // console.log(words);
+    words.forEach(word => {
+        const wordDiv = document.createElement('div');
+        wordDiv.innerHTML = `
+            <div
+            class="bg-white rounded-xl shadow-sm text-center py-20 px-5 space-y-6"
+        >
+            <h2 class="text-3xl font-bold">${word.word}</h2>
+            <p class="text-xl font-medium">Meaning /Pronounciation</p>
+            <div class="font-bangla text-3xl font-bold">"${word.meaning} / ${word.pronunciation}"</div>
+      </div>
+        `;
+        wordContainer.appendChild(wordDiv);
+    })
+    // clear wordContainer section
+    // wordContainer.innerHTML = '';
+}
+
 // show all levels
 const displayLevels = levels => {
     // get the levels container
@@ -19,7 +54,7 @@ const displayLevels = levels => {
         const levelDiv = document.createElement('div');
         // set the innerHTML
         levelDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary">
+            <button onclick='loadLevelWords(${level.level_no})' class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open"></i> 
                 Lesson - ${level.level_no}
             </button>
